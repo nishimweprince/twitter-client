e6406c6c9a6ed56f9369febbe7ff905afdeed0c6
Nishimwe Prince <princeelysee@gmail.com>
2025-01-20 16:43:13 +0200

    Enhance Twitter login process with improved logging and email support. Added debug and info logs to track login status and included email parameter in the login method for better authentication handling.

--- src/helpers/scraper.helper.ts
+++ src/helpers/scraper.helper.ts
@@ -1,7 +1,10 @@
 import { Scraper } from 'agent-twitter-client';
+import logger from './logger.heper';
 
 export const getScraper = async () => {
     const scraper = new Scraper();
-    await scraper.login(process.env.TWITTER_USERNAME as string, process.env.TWITTER_PASSWORD as string);
+    logger.debug(`Logging in to Twitter`);
+    await scraper.login(process.env.TWITTER_USERNAME as string, process.env.TWITTER_PASSWORD as string, process.env.TWITTER_EMAIL as string);
+    logger.info(`Logged in to Twitter`);
     return scraper;
 }
